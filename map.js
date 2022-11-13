aab4ce2eadc3a800d7d6500eb1c87502efc210fc
Jramirezzz <juanestebanrp123@gmail.com>
2022-11-12 21:12:38 -0500

    .

---
+++ map.js
@@ -0,0 +1,23 @@
+const map = document.getElementsByClassName(`mapa`)
+
+map.innerHTML += `<div>
+        <h1 class="M">Bind</h1>
+    </div>
+    <div class="parrafo ">
+            <img class="Ra" src="https://as01.epimg.net/meristation/imagenes/2020/06/09/guia_pagina/1591713433_872765_1591713693_portada_normal.jpg" alt="">
+            <p class="JR"> Bind's 'unique' feature is that it doesn't contain a mid section, instead having two one-way teleporters. One takes players from A Short to B Short and the other takes players from B Long to A Lobby. <br> <br>
+
+                Players use teleporters by walking into the section of wall taken up by the teleport entrance. They will then instantly arrive in a small exit room which players leave by walking towards the door out of the room. This door is closed by default and can only be opened by players inside the exit room walking up to it, at which point they will open automatically. Players outside the exit room cannot open these doors, but they can be penetrated by gunfire. <br> <br>
+                
+                Other objects can also be sent through teleporters if they come into contact with the teleporter entrance. They will maintain any velocity they had before being teleported and can open the doors and leave the exit room should they travel far enough to trigger the door sensor. Objects include abilities, the spike, and dropped weapons. <br> <br>
+                
+                Whenever a teleporter is used by an agent or an object, all players on the map will be given an audio cue. <br>
+
+            </p>
+
+       <a href="#boton"><button class="boton">
+            <img src="Save.png" height="50px" width="50px" alt="">
+        </button></a>
+
+    </div>
+`
--- script.js
+++ script.js
@@ -1,76 +1,68 @@
 data = [
     {
-        "id": 18,
+        "id": 1,
         "title": "Ascent",
         "category": {
-            "id": 1,
         },
         "images": [
             "https://media.valorant-api.com/maps/7eaecc1b-4337-bbf6-6ab9-04b8f06b3319/splash.png"
         ]
     },
     {
-        "id": 21,
+        "id": 2,
         "title": "Split",
         "category": {
-            "id": 5,
         },
         "images": [
             "https://media.valorant-api.com/maps/d960549e-485c-e861-8d71-aa9d1aed12a2/splash.png",
         ]
     },
     {
-        "id": 22,
+        "id": 3,
         "title": "Fracture",
         "category": {
-            "id": 5,
         },
         "images": [
             "https://media.valorant-api.com/maps/b529448b-4d60-346e-e89e-00a4c527a405/splash.png"        ]
     },
     {
-        "id": 24,
+        "id": 4,
         "title": "Bind",
         "category": {
-            "id": 3,
         },
         "images": [
             "https://media.valorant-api.com/maps/2c9d57ec-4431-9c5e-2939-8f9ef6dd5cba/splash.png",
         ]
     },
     {
-        "id": 26,
+        "id": 5,
         "title": "Breeze",
         "category": {
-            "id": 3,
         },
         "images": [
             "https://media.valorant-api.com/maps/2fb9a4fd-47b8-4e7d-a969-74b4046ebd53/splash.png",
         ]
     },{
-        "id": 28,
+        "id": 6,
         "title": "Pearl",
         "category": {
-            "id": 3,
         },
         "images": [
             "https://media.valorant-api.com/maps/fd267378-4d1d-484f-ff52-77821ed10dc2/splash.png",
         ]
     },{
-        "id": 30,
+        "id": 7,
         "title": "Icebox",
         "category": {
-            "id": 3,
         },
         "images": [
             "https://media.valorant-api.com/maps/e2ad5c54-4114-a870-9641-8ea21279579a/splash.png",
         ]
     },
     {
-        "id": 30,
+        "id": 8,
         "title": "Heaven",
         "category": {
-            "id": 3,
         },
         "images": [
             "https://media.valorant-api.com/maps/2bee0dc9-4ffe-519b-1cbd-7fbe763a6047/splash.png",
@@ -80,7 +72,7 @@ data = [
 
 const productsContainerDiv = document.getElementById("pcontainer")
 var productListHtml = ""
-// Create Product Class
+
 class Product {
     constructor(id, title, images) {
         this.id = id
@@ -97,7 +89,6 @@ class Product {
     }
 }
 
-// Create HTML component for product
 class ProductComponent {
     constructor(product, pos) {
         this.product = product
@@ -106,11 +97,9 @@ class ProductComponent {
 
     toHtml() {
         return `
-        <div class="product-card" onclick="productSelected(${this.pos}) onclick="OpenModal()">
+        <div class="product-card" onclick="productSelected(${this.pos})">
             <a href="map.html"><img alt="product" width="120" height="120" 
             src="${this.product.getFirstImage()}" /></a>
-            <div>
-            </div>
         </div>
         `
     }
@@ -131,12 +120,3 @@ function showAllProducts() {
 }
 
 showAllProducts()
-
-function OpenModal() {
-    let element = document.getElementById('overlay')
-    element.style.display = 'block'
-  }
-  function CloseModal() {
-    let element = document.getElementById('overlay')
-    element.style.display = 'none'
-  }
