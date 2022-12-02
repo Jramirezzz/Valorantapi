70cadf89aab88e373842b4bcf6e675b669201cd7
Jramirezzz <juanestebanrp123@gmail.com>
2022-12-01 20:25:19 -0500

    .

--- html/html/js/map.js
+++ html/html/js/map.js
@@ -1,23 +1,33 @@
-const map = document.getElementsByClassName(`mapa`)
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const valid = urlParams.get('uuid')
+const map = document.getElementById(`mapa`)
+const urlbase = 'https://valorant-api.com/v1/maps/'
 
-map.innerHTML += `<div>
-        <h1 class="M">Bind</h1>
+async function getmapInfo(url){
+    let response = await fetch(url);
+    let data = await response.json();
+
+    console.log(data)
+
+
+map.innerHTML += `
+<div>
+        <h1 class="M">${data.data.displayName}</h1>
     </div>
     <div class="parrafo ">
-            <img class="Ra" src="https://as01.epimg.net/meristation/imagenes/2020/06/09/guia_pagina/1591713433_872765_1591713693_portada_normal.jpg" alt="">
-            <p class="JR"> Bind's 'unique' feature is that it doesn't contain a mid section, instead having two one-way teleporters. One takes players from A Short to B Short and the other takes players from B Long to A Lobby. <br> <br>
-
-                Players use teleporters by walking into the section of wall taken up by the teleport entrance. They will then instantly arrive in a small exit room which players leave by walking towards the door out of the room. This door is closed by default and can only be opened by players inside the exit room walking up to it, at which point they will open automatically. Players outside the exit room cannot open these doors, but they can be penetrated by gunfire. <br> <br>
-                
-                Other objects can also be sent through teleporters if they come into contact with the teleporter entrance. They will maintain any velocity they had before being teleported and can open the doors and leave the exit room should they travel far enough to trigger the door sensor. Objects include abilities, the spike, and dropped weapons. <br> <br>
-                
-                Whenever a teleporter is used by an agent or an object, all players on the map will be given an audio cue. <br>
-
-            </p>
-
+            <img class="Ra" src="${data.data.splash}" alt="">
+            <p class="JR">${data.data.displayName} es un mapa que está en las coordenadas ${data.data.coordinates} en donde dos equipos luchan a muerte por defender o atacar los sites A (en las cordenadas X ${data.data.callouts[9].location.x} Y ${data.data.callouts[9].location.y}) y B (en las cordenadas X ${data.data.callouts[19].location.x} Y ${data.data.callouts[19].location.y}) </p>
+            <img class="Ram" src="${data.data.displayIcon}">
        <a href="#boton"><button class="boton">
-            <img src="Save.png" height="50px" width="50px" alt="">
+            <img src="imagen/Save.png" height="50px" width="50px" alt="">
         </button></a>
-
     </div>
 `
+}
+async function getallmapInfo(){
+    let url = urlbase + valid;
+    await getmapInfo(url)
+}
+
+getallmapInfo()
--- html/html/js/script.js
+++ html/html/js/script.js
@@ -1,18 +1,21 @@
-const fetchData = async () => {
+const productsContainerDiv = document.getElementById("pcontainer")
+var productListHtml = ""
+
+function fetchAsync() {
     const url = 'https://valorant-api.com/v1/maps'
-    // Get the list from the api
-    const data = await fetch(url)
-    data.data.forEach(information => {
-        document.getElementById("pcontainer").innerHTML += `
-        <img id="img11"
-      src="${information.imgUrl}" onclick="movieSelected('${movie.nombre}','${movie.Descripcion}','${movie.imgUrl}')">
-
-        ` 
-      })
-    // Convert to json
-    const dataJson = await data.json()
-    // Print the results
-    console.log(dataJson)
+    console.log(url)
+    fetch(url)
+        .then(response => {
+            response.json().then(data => {
+                data.data.forEach(information => {
+                    document.getElementById("pcontainer").innerHTML += `
+                    <a href="map.html?uuid=${information.uuid}"><img class="img" src="${information.splash}"" ></a>
+                    `
+                  })
+                console.log(data)
+                
+            })
+        })
+        
 }
-fetchData()
-
+fetchAsync()
