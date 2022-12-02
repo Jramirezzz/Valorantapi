08248657b22dc8bfaa8a0604768c83062151363a
Juan Esteban Ramirez Perdomo <115906656+Jramirezzz@users.noreply.github.com>
2022-12-01 21:33:23 -0500

    Actualizar val1.2.js

--- html/html/js/val1.2.js
+++ html/html/js/val1.2.js
@@ -1,8 +1,45 @@
-function OpenModal() {
-    let element = document.getElementById('overlay')
-    element.style.display = 'block'
-  }
-  function CloseModal() {
-    let element = document.getElementById('overlay')
-    element.style.display = 'none'
-  }
+const storage = window.localStorage
+
+function login() {
+    const loggedUser = storage.getItem("user")
+    const user = JSON.parse(loggedUser)
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+
+    //Comprobante de que todos los espacios se llenen con la información adecuada 
+
+    if (email == ""){
+        alert("You have to write an Email");
+        return true;
+    }
+    if (password == ""){
+        alert("You have to write a Password");
+        return true;
+    }
+    if (user.email===email){
+
+    }else{
+        alert("Your email is wrong")
+        return true;
+    }
+    if (user.password===password){
+
+    }else{
+        alert("Your password is wrong")
+        return true;
+    }
+    if(user.email === email && user.password === password) {
+        // Actualizo al usuario como "Logueado"
+        user.isLogged = true
+        const userJsonStr = JSON.stringify(user)
+        storage.setItem("user", userJsonStr)
+        
+        // Navego a la main
+        window.location.href = './LP 2.html'
+    }else{
+        alert("You are not registered")
+        return true;
+    }
+    // TODO: Handle error
+
+}
